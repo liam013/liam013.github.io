562bc02c6614760d695d78b6281e16e0e3c43bac
Liam M <liam013>
2016-03-05 16:46:26 -0500

    added demo

---
+++ demo/root/js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // always check inspect elements > console log 
+    console.log( 'jQuery started.' );
+    
+    $('.subMenu').smint({
+    	'scrollSpeed' : 1000
+    });
+
+});
