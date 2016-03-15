8565eb641309c522914f079a5f321af7b6850b52
Liam M <liam013>
2016-03-15 09:30:42 -0400

    animate fade

--- js/getNav.js
+++ js/getNav.js
@@ -2,7 +2,7 @@ $.get("http://liam013.github.io/partials/nav.html", function(data){
 
     $(document).ready(function(){
        // $(".container"){display:none;}
-        $(".container").style.display = "none";
+        $(".container").html(response).css({'opacity':0}).animate({'opacity':1});
     $(".container").prepend(data);
         
    // $("body").prepend(data);
