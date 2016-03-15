1bf6cffe8b59614006117c8143f31d2098ff2df5
Liam M <liam013>
2016-03-15 11:40:53 -0400

    delete week5 stray html

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -1,4 +1,20 @@
+
+$(docuement).ready(function(){
+    
 $("#getClients").on("click", function(){
 
-alert("working");
+var url = "http://liam013.github.io/otherProjects/ajaxEamples/clients.json"
+$.getJSON(url, function(data){
+
+    $.each(data, function(index, item){
+    $("#data").append(item.name);
+    })
+    //alert(data);
+    //console.dir(data);
+    
+//JSON ends
+})
+//click function ends
+})
+//document ready ends
 })
