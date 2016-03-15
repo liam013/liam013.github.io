a62f187a1fc103e4527d124e995dab2a184c577d
Liam M <liam013>
2016-03-15 09:33:45 -0400

    hide > fadeIn

--- js/getNav.js
+++ js/getNav.js
@@ -2,7 +2,7 @@ $.get("http://liam013.github.io/partials/nav.html", function(data){
 
     $(document).ready(function(){
        // $(".container"){display:none;}
-        $(".container").html(response).css({'opacity':0}).animate({'opacity':1});
+        $(".container").hide();
     $(".container").prepend(data);
         
    // $("body").prepend(data);
