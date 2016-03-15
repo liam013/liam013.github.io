e73e99a6dc493e3b8ff39acbc2509d617d58d38d
Liam M <liam013>
2016-03-15 11:43:58 -0400

    changed order of script tags

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -4,6 +4,7 @@ $(docuement).ready(function(){
 $("#getClients").on("click", function(){
 
 var url = "http://liam013.github.io/otherProjects/ajaxEamples/clients.json"
+
 $.getJSON(url, function(data){
 
     $.each(data, function(index, item){
