9c09187512de7e2037255f9392b93f9ba1a7c16f
Liam M <liam013>
2016-03-15 10:32:20 -0400

    oops

--- js/getNav.js
+++ js/getNav.js
@@ -2,11 +2,14 @@ $.get("http://liam013.github.io/partials/nav.html", function(data){
 
     $(document).ready(function(){
       
-        $(".container").hide();
+    $(".container").hide();
     $(".container").prepend(data);
-        
-
-        $(".container").fadeIn(800);
+    $(".container").fadeIn(800);
+    
+  $.get("http://liam013.github.io/partials/footer.html", function(data){  
+document.getElementsByClassName(".row footer"); 
+         $(".container").append(".row footer");
     })
 
+    })
 })
