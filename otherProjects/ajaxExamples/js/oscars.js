ee70affe6f09db7f0ddcf07a0125e98e2f0d0d5c
Liam M <liam013>
2016-03-15 18:32:55 -0400

    quotes

--- otherProjects/ajaxExamples/js/oscars.js
+++ otherProjects/ajaxExamples/js/oscars.js
@@ -13,7 +13,7 @@ var html = "<table class='table table-hover table-striped'>" +
     "<td>" + item.year + "</td>"+
     "<td>" + item.title + "</td>" + 
     "<td>" + item.director + "</td>" +
-    "<td>" + input id="agree" name="numberOfBlanks" type="checkbox" value="" /> + "</td>" +
+    "<td>" + "<input id='agree' name='numberOfBlanks' type='checkbox' value='' />" + "</td>" +
     "</tr>";
         
     })
