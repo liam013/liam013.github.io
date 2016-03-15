42cf4005cceff91233f97a7c5347c643cd60c0fa
Liam M <liam013>
2016-03-15 12:09:18 -0400

    updated clients

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -5,11 +5,11 @@ $("#getClients").on("click", function(){
 var url = "http://liam013.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json"
 
 $.getJSON(url, function(data){
-var html = "<table>"+
+var html = "<table class='table table-hover table-striped'>" +
     "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
     $.each(data, function(index, item){
     //$("#data").append(item.name);
-    html+ = "<tr>" +
+    html += "<tr>" +
     "<td>" + item.name + "</td>"+
     "<td>" + item.email + "</td>" + 
     "<td>" + item.company + "</td>" +
