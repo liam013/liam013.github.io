f44654fa11da65d6300bcbcd323404a3d515d1fe
Liam M <liam013>
2016-03-15 12:39:06 -0400

    show footer hide footer

--- js/getNav.js
+++ js/getNav.js
@@ -1,12 +1,12 @@
 $.get("http://liam013.github.io/partials/nav.html", function(data){
 
     $(document).ready(function(){
-      
+    $(".row footer").hide();
     $(".container").hide();
     $(".container").prepend(data);
     $(".container").fadeIn(800); 
 
-    $(".row footer").hide();
+   
     })
 
 
