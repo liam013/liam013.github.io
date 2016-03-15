e0e595700d768255703bd7d9ec1f08dc21db9d0a
Liam M <liam013>
2016-03-15 09:17:15 -0400

    fade in

--- js/getNav.js
+++ js/getNav.js
@@ -3,8 +3,8 @@ $.get("http://liam013.github.io/partials/nav.html", function(data){
     $(document).ready(function(){
     $(".container").prepend(data);
    // $("body").prepend(data);
-    
-
+    //.container.fadeIn();
+        $(".container").fadeIn();
     })
 
 })
