7969f299cf46d4ae5a6e6d062bf9ee68312114a9
Liam M <liam013>
2016-03-15 08:55:34 -0400

    partial nav

---
+++ js/getNav.js
@@ -0,0 +1,10 @@
+$.get("http://liam013.github.io/partials/nav.html", function(data){
+
+    $(document).ready(function(){
+    $(".container").prepend(data);
+   // $("body").prepend(data);
+    
+
+    })
+
+})
