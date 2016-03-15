b72b902bde031abf4d6170665227952201758c91
Liam M <liam013>
2016-03-15 12:41:36 -0400

    footer hide

--- js/getFooter.js
+++ js/getFooter.js
@@ -1,7 +1,8 @@
-$.get("http://liam013.github.io/partials/nav.html", function(data){
 
-    $(document).ready(function(){
+$.get("http://liam013.github.io/partials/nav.html", function(data){
     $(".row footer").show();
+    $(document).ready(function(){
+    
     $(".container").append.getElementsByClassName(".row footer");
     
     })
