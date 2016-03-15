a8ce3da54acd59aa5ad7ad8f6bc9899a198bdf3d
Liam M <liam013>
2016-03-15 11:06:17 -0400

    ajax json added

--- js/getNav.js
+++ js/getNav.js
@@ -5,7 +5,7 @@ $.get("http://liam013.github.io/partials/nav.html", function(data){
     $(".container").hide();
     $(".container").prepend(data);
     $(".container").fadeIn(800); 
-
+//make getFooter.js if he wants seperate .js files but only one html file
     $(".container").append.getElementsByClassName(".row footer");
 
     })
