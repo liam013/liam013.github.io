b3970e605cc0c285c39c3d7a774be5e5f6355147
Liam M <liam013>
2016-03-15 18:57:05 -0400

    alert

--- otherProjects/ajaxExamples/js/oscars.js
+++ otherProjects/ajaxExamples/js/oscars.js
@@ -23,7 +23,7 @@ var html = "<table class='table table-hover table-striped'>" +
     
 $("#compare").on("click", function(){
     var choice = ($('.agree:checked').length / 16) * 100;
-    alert("You agree with " + $('.agree:checked').length + " of the choices" + " which is "+ choice+ "%" );
+    alert("You agree with " + $('.agree:checked').length + " of the choices," + " which is "+ choice+ "%" );
 });
     
 //JSON ends
