29b8635bbb119a27acbcb154866025f522d06216
Liam M <liam013>
2016-03-15 18:49:56 -0400

    spacing

--- otherProjects/ajaxExamples/js/oscars.js
+++ otherProjects/ajaxExamples/js/oscars.js
@@ -23,7 +23,7 @@ var html = "<table class='table table-hover table-striped'>" +
     
 $("#compare").on("click", function(){
     var choice = ('.agree:checked'.length / 15);
-    alert("You agree with " + $('.agree:checked').length + "of the choices");
+    alert("You agree with " + $('.agree:checked').length+ "of the choices");
 });
     
 //JSON ends
