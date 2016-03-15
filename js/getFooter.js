f98b93a60152712758c16ce846d0e51d143fd355
Liam M <liam013>
2016-03-15 11:16:15 -0400

    added getFooter js

---
+++ js/getFooter.js
@@ -0,0 +1,9 @@
+$.get("http://liam013.github.io/partials/nav.html", function(data){
+
+    $(document).ready(function(){
+    $(".container").append.getElementsByClassName(".row footer");
+
+    })
+
+
+})
