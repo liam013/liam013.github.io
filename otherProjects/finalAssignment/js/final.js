001b7be8ed39b514133fe8179c80bbaba43b1176
Liam M <liam013>
2016-03-29 22:38:30 -0400

    final prj

---
+++ otherProjects/finalAssignment/js/final.js
@@ -0,0 +1,14 @@
+var x = document.getElementById("myAudio"); 
+
+function playAudio() { 
+    x.play(); 
+    alert("Music is playing");
+ 
+} 
+
+function pauseAudio() { 
+    x.pause(); 
+    alert("Music is paused");
+  
+} 
+
